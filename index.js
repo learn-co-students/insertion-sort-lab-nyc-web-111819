7a821223a960f456ab7e2014ca61d3d04ed47126
Sean Tarzy <seantarzy@gmail.com>
2020-04-02 16:15:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+    let minNumber = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++){
+    if(array[i] < minNumber){
+        minNumber = array[i]
+        minIndex = i
+    }
+}
+array.splice(minIndex, 1)
+return minNumber
+
 
 }
 
 function insertionSort(array){
-
+    let newMin
+    let sorted = []
+    while(array.length != 0 ){
+     newMin = findMinAndRemove(array)
+    sorted.push(newMin)
+    }
+    return sorted
 }
